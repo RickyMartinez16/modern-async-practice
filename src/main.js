af744cfa1b7b2107e95b54f19e3cd7f04446bd72
Ricky Martinez <RMartinezW16@gmail.com>
2022-08-05 15:40:40 -0400

    main

--- src/main.js
+++ src/main.js
@@ -5,8 +5,33 @@ function isValid({ id, name, meaning, quadrant, starsWithPlanets }) {
   return id && name && meaning && quadrant && starsWithPlanets;
 }
 
-function update(constellation) {}
+async function update(constellation) {
+  try {
+    const url = `${BASE_URL}/constellations/${constellation.id}`;
+    const result = await axios.put(url, constellation);
+    return result;
+  } catch(err){
+    return `Updating constellation (id: ${id}) failed.`
+  }
+}
 
-function bulkImport(constellations) {}
+async function bulkImport(constellations) {
+  try {
+    if (!Array.isArray(constellations)) {
+      return "Inputted argument must be an array."
+    };
+    if (constellations.every((constellation) => isValid(constellation))) {
+      return Promise.allSettled(
+        constellations.map((constellation) => update(constellation))
+      );
+    }
+    else {
+      return "All constellations must include relevant fields."
+    }
+  } 
+  catch(error) {
+    console.log(error)
+  }
+}
 
 module.exports = { bulkImport, update };
